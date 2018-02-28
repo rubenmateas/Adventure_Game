212b4ea487c685e6e24b1d2f48a5d4b85cc6e78b
Ruben Mateas <rubenmaetas1313@outlook.com>
2018-02-28 03:49:42 -0800

    removed unnecessary js file

--- app.js
+++ app.js
@@ -1,194 +1,93 @@
-let north = 0;
-let east = 0;
-let south = 0;
-let west = 0;
-let pos = 'Entrance';
-let world = 0;
-let inventory = [];
+let north = document.getElementById('northBtn').value = 0;
+let west = document.getElementById('westBtn').value = 0;
+let east = document.getElementById('eastBtn').value = 0;
+let south = document.getElementById('southBtn').value = 0;
+let mapSize = 0;
+let btn = document.getElementsByClassName('btn');
+let playerPos = north + ',' + west + ',' + east + ',' + south;
+let inv = [];
 
+function Item(name,description,position) {
+  this.name = name,
+  this.description = description,
+  this.position = position
+}
 
-//Items for the player to pick up
+let items = [
+  new Item('Flashlight','Used to see in dark places','north == 0 && south == 0 && east == 0 && west == 1'),
+  new Item('Flower','Present to the princess to wake her up','north == 0 && south == 0 && east == 1 && west == 0'),
+  new Item('Key','Allows access to the castle','north == 0 && south == 0 && east == 0 && west == 0')
+];
 
-let flashlight = {
-  name: 'Flashlight',
-  description: 'Used to see in dark places',
-  location: 'north == 0 && south == 0 && east == 0 && west == 1'
+function Room(name, position, imgSrc, hasItem, displayItem){
+  this.name = name,
+  this.position = position,
+  this.imgSrc = imgSrc
+}
+
+let rooms = [
+  new Room('Armory','0,1,0,0','./images/armory.jpg',null),
+  new Room('Control Room','1,0,1,0','./images/controlroom.jpg','yes'),
+  new Room('Dungeon','1,1,0,0','./images/dungeon.jpg',null),
+  new Room('Entrance','0,0,0,0','./images/entrance.jpg',null),
+  new Room('Fighting Hall','1,0,0,0','./images/fightinghall.jpg',null),
+  new Room('Garden','0,0,1,0','./images/garden.jpg','yes'),
+  new Room('Princess','0,0,0,1','./images/princess.jpg',null),
+  new Room('Stockroom','0,0,1,1','./images/stockroom.jpg','yes'),
+  new Room('Wine Cellar','0,1,0,1','./images/winecellar.jpg',null)
+];
+
+function direction(dir) {
+  if(dir == 'north'){
+    if(north > mapSize){
+      return
+    }
+    if(south > 0){
+      south = south - 1;
+    }else{
+      north++;
+    }
+  }else if(dir == 'west'){
+    if(west > mapSize){
+      return
+    }
+    if(east > 0){
+      east = east - 1;
+    }else{
+      west++;
+    }
+  }else if(dir == 'east'){
+    if(east > mapSize){
+      return
+    }
+    if(west > 0){
+      west = west - 1;
+    }else{
+      east++;
+    }
+  }else if(dir == 'south'){
+    if(south > mapSize){
+      return
+    }
+    if(north > 0){
+      north = north - 1;
+    }else{
+      south++;
+    }
+  }
+  playerPos = north + ',' + west + ',' + east + ',' + south;
+  document.getElementById("play").innerHTML = "North: " + north + "   South: " + south + "   East: " + east + "   West: " + west;
+  changeSetting();
 };
 
-let flower = {
-  name: 'Flower',
-  description: 'Present to the princess to wake her up',
-  location: 'north == 0 && south == 0 && east == 1 && west == 0'
-};
-
-let key = {
-  name: 'Key',
-  description: 'Allows access to the castle',
-  location: 'north == 0 && south == 0 && east == 0 && west == 0'
-};
-
-let item = function(itemName){
-  let thing = {};
-  thing.name = name;
-  thing.description = description;
-};
-
-//Rooms
-
-let armory = {
-  name: 'Armory',
-  coordinates: 'north == 0 && south == 1 && east == 0 && west == 0',
-  imgSrc: './images/armory.jpg'
-}
-
-let controlroom = {
-  name: 'Control Room',
-  coordinates: 'north == 1 && south == 0 && east == 1 && west == 0',
-  imgSrc: './images/controlroom.jpg'
-}
-
-let dungeon = {
-  name: 'Dungeon',
-  coordinates: 'north == 1 && south == 0 && east == 0 && west == 1',
-  imgSrc: './images/dungeon.jpg'
-}
-
-let entrance = {
-  name: 'Entrance',
-  coordinates: 'north == 0 && south == 0 && east == 0 && west == 0',
-  imgSrc: './images/entrance.jpg'
-}
-
-let fightinghall = {
-  name: 'Fighting Hall',
-  coordinates: 'north == 1 && south == 0 && east == 0 && west == 0',
-  imgSrc: './images/fightinghall.jpg'
-}
-
-let garden = {
-  name: 'Garden',
-  coordinates: 'north == 0 && south == 0 && east == 1 && west == 0',
-  imgSrc: './images/garden.jpg'
-}
-
-let princess = {
-  name: 'Princess',
-  coordinates: 'north == 0 && south == 1 && east == 1 && west == 0',
-  imgSrc: './images/princess.jpg'
-}
-
-let stockroom = {
-  name: 'Stockroom',
-  coordinates: 'north == 0 && south == 0 && east == 0 && west == 1',
-  imgSrc: './images/stockroom.jpg'
-}
-
-let winecellar = {
-  name: 'Wine Cellar',
-  coordinates: 'north == 0 && south == 1 && east == 0 && west == 1',
-  imgSrc: './images/winecellar.jpg'
-}
-
-let rooms = [armory, controlroom, dungeon, entrance, fightinghall, garden, princess, stockroom, winecellar];
-
-//Buttons to handle events
-
-function take() {
-
-}
-
-//Directions player can make
-function direction1(){
-  if(north > world){
-    return;
-  }
-  if(south < 1){
-    north = north + 1;
-  }else{
-    south = south - 1;
-  }
-  myplayer();
-  document.getElementById("play").innerHTML =
-    "North: " + north + "   South: " + south + "   East: " + east + "   West: " + west + "<br>" + "Location: " + pos;
-}
-
-
-function direction2(){
-  if(west > world){
-    return;
-  }
-  if(east < 1){
-    west = west + 1;
-  }else{
-    east = east - 1;
-  }
-  myplayer();
-  document.getElementById("play").innerHTML =
-    "North: " + north + "   South: " + south + "   East: " + east + "   West: " + west + "<br>" + "Location: " + pos;
-}
-
-function direction3(){
-  if(east > world){
-    return;
-  }
-  if(west < 1){
-    east = east + 1;
-  }else{
-    west = west - 1;
-  }
-  myplayer();
-  document.getElementById("play").innerHTML =
-  "North: " + north + "   South: " + south + "   East: " + east + "   West: " + west + "<br>" + "Location: " + pos;
-}
-
-
-function direction4(){
-  if(south > world){
-    return;
-  }
-  if(north > 0){
-    north = north - 1;
-  }else{
-    south = south + 1;
-  }
-  myplayer();
-  document.getElementById("play").innerHTML =
-    "North: " + north + "   South: " + south + "   East: " + east + "   West: " + west + "<br>" + "Location: " + pos;
-}
-
-
-//Result of the moves the player makes
-function myplayer() {
+function changeSetting() {
   let image = document.getElementById('locationImg');
-  if(north == 0 && south == 0 && east == 0 && west == 0){
-    pos = "Entrance";
-    image.src = "./images/entrance.jpg";
-  }else if(north == 0 && south == 0 && east == 1 && west == 0){
-    pos = "Garden";
-    image.src = "./images/garden.jpg";
-  }else if(north == 0 && south == 0 && east == 0 && west == 1){
-    pos = "Stock Room";
-    image.src = "./images/stockroom.jpg";
-  }else if(north == 1 && south == 0 && east == 0 && west == 0){
-    pos = "Fighting Hall";
-    image.src = "./images/fightinghall.jpg";
-  }else if(north == 1 && south == 0 && east == 1 && west == 0){
-    pos = "Moat Control Room";
-    image.src = "./images/controlroom.jpg";
-  }else if(north == 1 && south == 0 && east == 0 && west == 1){
-    pos = "Dungeon";
-    image.src = "./images/dungeon.jpg";
-  }else if(north == 0 && south == 1 && east == 0 && west == 0){
-    pos = "Armory";
-    image.src = "./images/armory.jpg";
-  }else if(north == 0 && south == 1 && east == 1 && west == 0){
-    pos = "Princess Chambers";
-    image.src = "./images/princess.jpg";
-  }else if(north == 0 && south == 1 && east == 0 && west == 1){
-    pos = "Wine Cellar";
-    image.src = "./images/winecellar.jpg";
-  }else{
-    pos = "How did you get this lost";
-    image.src = "./images/boi.jpg";
+  let loc = document.getElementById('loc');
+  for (var i = 0; i < rooms.length; i++) {
+    if(rooms[i].position == playerPos) {
+      image.src = rooms[i].imgSrc;
+      loc.innerHTML = 'Location: ' + rooms[i].name;
+      break;
+    }
   }
 }
